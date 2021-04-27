cdbec1307090e86e5bafd230742bb6e7d586bc83
noha nomier <nnomier@gmail.com>
2021-04-27 23:55:13 +0200

    add starter code fyuur project

---
+++ project01_fyyur/config.py
@@ -0,0 +1,15 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nomier:noha97@localhost:5432/fyyur'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
